74c1e9b439a6e0fd1a5f7e6da1c65392362a29be
Manuel Blanc Casado <e283524@3-12-65-116.(none)>
2015-03-12 17:19:13 +0100

    Borrado setup viejo, cambiado colores terminal

--- .eps-dotfiles-extra/setup.sh
+++ .eps-dotfiles-extra/setup.sh
@@ -2,6 +2,7 @@
 
 cd ~
 
+
 ## Clonar repositorio git
 git init .
 git remote add origin https://github.com/ManuelBlanc/eps-dotfiles.git
@@ -13,18 +14,21 @@ git checkout -f master
 gconftool-2 --set '/apps/gnome-terminal/profiles/Default/default_size_columns'   	--type int '100'
 gconftool-2 --set '/apps/gnome-terminal/profiles/Default/default_size_rows'      	--type int '30'
 gconftool-2 --set '/apps/gnome-terminal/profiles/Default/use_custom_default_size'	--type bool 'true'
-gconftool-2 --set '/apps/gnome-terminal/profiles/Default/font'                   	--type string 'Monospace 11'
+gconftool-2 --set '/apps/gnome-terminal/profiles/Default/font'                   	--type string 'DejaVu Sans Mono 14'
 gconftool-2 --set '/apps/gnome-terminal/profiles/Default/use_system_font'        	--type bool 'false'
 gconftool-2 --set '/apps/gnome-terminal/profiles/Default/default_show_menubar'   	--type bool 'false'
 gconftool-2 --set '/apps/gnome-terminal/profiles/Default/use_theme_colors'       	--type bool 'false'
-gconftool-2 --set '/apps/gnome-terminal/profiles/Default/palette'                	--type string '#2E2E34343636:#CCCC00000000:#4E4E9A9A0606:#C4C4A0A00000:#34346565A4A4:#757550507B7B:#060698209A9A:#D3D3D7D7CFCF:#555557575353:#EFEF29292929:#8A8AE2E23434:#FCFCE9E94F4F:#72729F9FCFCF:#ADAD7F7FA8A8:#3434E2E2E2E2:#EEEEEEEEECEC'
+gconftool-2 --set '/apps/gnome-terminal/profiles/Default/palette'                	--type string '#000000000000:#EFEF29292929:#8A89E2E13433:#FCFCE9E84F4F:#FCFCAFAF3E3D:#ADAD7F7FA8A7:#72729F9FCFCF:#D3D3D7D7CFCF:#555557575353:#A4A400000000:#4E4E9A990605:#C4C4A09F0000:#CECE5C5B0000:#5C5B35356666:#201F4A4A8787:#EEEEEEEEECEC'
 gconftool-2 --set '/apps/gnome-terminal/profiles/Default/alternate_screen_scroll'	--type bool 'true'
-gconftool-2 --set '/apps/gnome-terminal/profiles/Default/background_color'       	--type string '#000000'
+gconftool-2 --set '/apps/gnome-terminal/profiles/Default/background_color'       	--type string '#000000000000'
 gconftool-2 --set '/apps/gnome-terminal/profiles/Default/visible_name'           	--type string 'Predeterminado'
 gconftool-2 --set '/apps/gnome-terminal/profiles/Default/bold_color'             	--type string '#000000000000'
-gconftool-2 --set '/apps/gnome-terminal/profiles/Default/foreground_color'       	--type string '#FFFFFF'
+gconftool-2 --set '/apps/gnome-terminal/profiles/Default/foreground_color'       	--type string '#F3F2F3F2F3F2'
 
 >&2 echo 'Traido ficheros del repositorio y cambiado la configuracion de la terminal' 
 >&2 echo 'Es posible que sea necesario abrir una terminal nueva para que los cambios'
 >&2 echo 'entren en efecto.' 
+
+# Intentamos ejecutar el .bashrc
+. .bashrc
 
